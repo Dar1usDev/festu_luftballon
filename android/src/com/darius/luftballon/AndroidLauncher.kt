2362fc42f143adba7485bce74597d9038f79f2f4
Dmitry <Selesnjow@gmail.com>
2020-06-15 01:59:57 +1000

    Initial commit

---
+++ android/src/com/darius/luftballon/AndroidLauncher.kt
@@ -0,0 +1,21 @@
+package com.darius.luftballon
+
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.darius.luftballon.Activities.StartActivity
+
+
+class AndroidLauncher : AndroidApplication() {
+    private var spen: SPen? = null
+
+    private var startActivity: StartActivity? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        startActivity = StartActivity()
+        initialize(startActivity, config)
+        //spen = SPen(startActivity!!)
+    }
+}
---
+++ android/src/com/darius/luftballon/SPenJ.java
@@ -0,0 +1,22 @@
+package com.darius.luftballon;
+
+import android.view.KeyEvent;
+
+import com.darius.luftballon.Activities.StartActivity;
+
+public class SPenJ {
+    private static StartActivity simulation;
+
+    public SPenJ(){
+
+    }
+    public SPenJ(StartActivity simulation){
+        this.simulation = simulation;
+    }
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//
+//        return super.onKeyDown(keyCode, event);
+//    }
+}
